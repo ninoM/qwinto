f528a69afa4e747147168d80e1266e7bb428f8fa
Niño Mollaneda <ninom@Mr-Meeseeks.local>
2023-04-07 21:23:11 +0800

    Handle layout for larger screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import { FaultButton } from "../components/FaultButton";
 
 const Input = ({ special, empty }: { special?: true; empty?: true }) => {
-  if (empty) return <div className="w-8 h-8" />;
+  if (empty) return <div className="h-8 w-8" />;
   return (
-    <div className="rounded-full text-black ring-1 ring-slate-400 overflow-hidden">
+    <div className="overflow-hidden rounded-full text-black ring-1 ring-slate-400">
       <input
         type="text"
         pattern="[0-9]*"
-        className={`w-8 h-8 text-center ${special ? "bg-yellow-200" : ""}`}
+        className={`h-8 w-8 text-center ${special ? "bg-yellow-200" : ""}`}
       />
     </div>
   );
@@ -22,18 +22,17 @@ const Group = ({
 }) => {
   return (
     <div
-      className={`border w-12 border-slate-600 rounded py-2 flex place-items-center place-content-center flex-col gap-y-2 ${className}`}
+      className={`flex w-12 flex-col place-content-center place-items-center gap-y-2 rounded border border-slate-600 p-2  md:w-auto md:flex-row md:gap-x-2 ${className}`}
     >
       {children}
     </div>
   );
 };
 
-
 export default function Home() {
   return (
     <main className="flex flex-col p-2">
-      <div className="flex flex-row items-start justify-center gap-x-2 p-2">
+      <div className="flex flex-row items-start justify-center gap-x-2 p-2 md:flex-col-reverse md:items-center md:justify-stretch md:gap-y-2">
         <Group className="bg-violet-600">
           <Input />
           <Input />
@@ -46,7 +45,7 @@ export default function Home() {
           <Input />
           <Input special />
         </Group>
-        <Group className="mt-[42px] bg-yellow-500">
+        <Group className="mt-[42px] bg-yellow-500 md:ml-[82px] md:mt-auto">
           <Input />
           <Input />
           <Input />
@@ -58,7 +57,7 @@ export default function Home() {
           <Input />
           <Input />
         </Group>
-        <Group className="mt-[84px] bg-orange-500">
+        <Group className="mt-[84px] bg-orange-500 md:ml-[164px] md:mt-auto">
           <Input />
           <Input special />
           <Input />
@@ -71,7 +70,7 @@ export default function Home() {
           <Input />
         </Group>
       </div>
-      <div className="self-center flex items-center flex-col">
+      <div className="flex flex-col items-center self-center">
         <p>FAULTS</p>
         <div className="flex gap-x-2">
           <FaultButton />
