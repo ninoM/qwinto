3977c81770f01aeeadbffa24fe17e0cdca094917
Niño Mollaneda <ninom@Mr-Meeseeks.local>
2023-04-07 19:20:28 +0800

    base layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,73 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+const Input = ({ special, empty }: { special?: true; empty?: true }) => {
+  if (empty) return <div className="w-8 h-8" />;
+  return (
+    <div className="rounded-full text-black outline outline-1 outline-slate-400 overflow-hidden">
+      <input
+        type="text"
+        pattern="[0-9]*"
+        className={`w-8 h-8 text-center ${
+          special ? "bg-yellow-200" : ""
+        }`}
+      />
+    </div>
+  );
+};
 
-const inter = Inter({ subsets: ['latin'] })
+const Group = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={`border w-12 border-slate-600 rounded py-2 flex place-items-center place-content-center flex-col gap-y-2 ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className='border w-5 h-5 border-slate-600'></div>
+    <main className="flex min-h-screen flex-row items-center justify-center gap-x-2 p-2">
+      <Group className="bg-violet-600">
+        <Input />
+        <Input />
+        <Input special />
+        <Input />
+        <Input empty />
+        <Input />
+        <Input />
+        <Input />
+        <Input />
+        <Input special />
+      </Group> 
+       <Group className="mt-[85px] bg-yellow-500">
+        <Input />
+        <Input />
+        <Input />
+        <Input />
+        <Input />
+        <Input empty />
+        <Input />
+        <Input special />
+        <Input />
+        <Input />
+      </Group>
+       <Group className="mt-[170px] bg-orange-500">
+        <Input />
+        <Input special/>
+        <Input />
+        <Input empty />
+        <Input />
+        <Input special />
+        <Input />
+        <Input />
+        <Input />
+        <Input />
+      </Group>
     </main>
-  )
+  );
 }
