bc047318b587f8fb25d64898cb4c78871cc0992f
Niño Mollaneda <ninom@Mr-Meeseeks.local>
2023-04-07 20:15:09 +0800

    Add fault buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { FaultButton } from "./FaultButton";
+
 const Input = ({ special, empty }: { special?: true; empty?: true }) => {
   if (empty) return <div className="w-8 h-8" />;
   return (
@@ -5,9 +7,7 @@ const Input = ({ special, empty }: { special?: true; empty?: true }) => {
       <input
         type="text"
         pattern="[0-9]*"
-        className={`w-8 h-8 text-center ${
-          special ? "bg-yellow-200" : ""
-        }`}
+        className={`w-8 h-8 text-center ${special ? "bg-yellow-200" : ""}`}
       />
     </div>
   );
@@ -29,45 +29,57 @@ const Group = ({
   );
 };
 
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-row items-start justify-center gap-x-2 p-2">
-      <Group className="bg-violet-600">
-        <Input />
-        <Input />
-        <Input special />
-        <Input />
-        <Input empty />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input special />
-      </Group> 
-       <Group className="mt-[42px] bg-yellow-500">
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input empty />
-        <Input />
-        <Input special />
-        <Input />
-        <Input />
-      </Group>
-       <Group className="mt-[84px] bg-orange-500">
-        <Input />
-        <Input special/>
-        <Input />
-        <Input empty />
-        <Input />
-        <Input special />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-      </Group>
+    <main className="flex flex-col p-2">
+      <div className="flex flex-row items-start justify-center gap-x-2 p-2">
+        <Group className="bg-violet-600">
+          <Input />
+          <Input />
+          <Input special />
+          <Input />
+          <Input empty />
+          <Input />
+          <Input />
+          <Input />
+          <Input />
+          <Input special />
+        </Group>
+        <Group className="mt-[42px] bg-yellow-500">
+          <Input />
+          <Input />
+          <Input />
+          <Input />
+          <Input />
+          <Input empty />
+          <Input />
+          <Input special />
+          <Input />
+          <Input />
+        </Group>
+        <Group className="mt-[84px] bg-orange-500">
+          <Input />
+          <Input special />
+          <Input />
+          <Input empty />
+          <Input />
+          <Input special />
+          <Input />
+          <Input />
+          <Input />
+          <Input />
+        </Group>
+      </div>
+      <div className="self-center flex items-center flex-col">
+        <p>FAULTS</p>
+        <div className="flex gap-x-2">
+          <FaultButton />
+          <FaultButton />
+          <FaultButton />
+          <FaultButton />
+        </div>
+      </div>
     </main>
   );
 }
